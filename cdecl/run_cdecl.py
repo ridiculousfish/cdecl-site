e2c12719385e1db4bd018c453638635654552117
ridiculousfish <corydoras@ridiculousfish.com>
2016-11-27 13:16:15 -0800

    Replace PHP with Python

---
+++ cdecl/run_cdecl.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+SYNTAX_ERROR="syntax error"
+
+def run_query(event, context):
+	query = event['q']
+	if not query:
+		return 'Bad query'
+	return run_3_cdecl(query)
+	
+
+def run_3_cdecl(query):
+	queries = [query, 'explain ' + query, 'declare ' + query]
+	path = "./exe/cdecl_" + ('darwin' if sys.platform == 'darwin' else 'linux')
+	proc = subprocess.Popen([path], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+	outdata, _ = proc.communicate('\n'.join(queries))
+	for line in outdata.split('\n'):
+		if line and line != SYNTAX_ERROR:
+			return line
+	else:
+		return SYNTAX_ERROR
+
+if __name__ == '__main__':
+	for arg in sys.argv[1:]:
+		print run_3_cdecl(arg)
