84ff59c8966409cde3f71f3bcb6613527b928c58
ridiculousfish <corydoras@ridiculousfish.com>
2016-11-27 13:38:07 -0800

    Tabs -> spaces

--- cdecl/run_cdecl.py
+++ cdecl/run_cdecl.py
@@ -6,23 +6,22 @@ import sys
 SYNTAX_ERROR="syntax error"
 
 def lambda_handler(event, context):
-	query = event['q']
-	if not query:
-		return 'Bad query'
-	return run_3_cdecl(query)
-	
+    query = event['q']
+    if not query:
+        return 'Bad query'
+    return run_3_cdecl(query)
 
 def run_3_cdecl(query):
-	queries = [query, 'explain ' + query, 'declare ' + query]
-	path = "./exe/cdecl_" + ('darwin' if sys.platform == 'darwin' else 'linux')
-	proc = subprocess.Popen([path], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-	outdata, _ = proc.communicate('\n'.join(queries))
-	for line in outdata.split('\n'):
-		if line and line != SYNTAX_ERROR:
-			return line
-	else:
-		return SYNTAX_ERROR
+    queries = [query, 'explain ' + query, 'declare ' + query]
+    path = "./exe/cdecl_" + ('darwin' if sys.platform == 'darwin' else 'linux')
+    proc = subprocess.Popen([path], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    outdata, _ = proc.communicate('\n'.join(queries))
+    for line in outdata.split('\n'):
+        if line and line != SYNTAX_ERROR:
+            return line
+    else:
+        return SYNTAX_ERROR
 
 if __name__ == '__main__':
-	for arg in sys.argv[1:]:
-		print run_3_cdecl(arg)
+    for arg in sys.argv[1:]:
+        print run_3_cdecl(arg)
